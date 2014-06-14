135b82e7a26ed236b297375a30b012533256f162
malnakli <hassoun@outlook.com>
2014-06-14 17:23:02 -0400

    Revert "Add main page to the project"
    This reverts commit 9c050382c9f9bd36a9bd3de1e128ae68ce22445f.

--- ship-network/app/controllers/main_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-=begin
-    This is the main pages of the ship network
-=end
-
-class MainPagesController < ApplicationController
-  
-    def index
-    end 
-end
--- ship-network/app/helpers/application_helper.rb
+++ ship-network/app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-    # this function return a string of the project name which is ship network to be used in views
-    
-    def project_name
-        "Ship Network"
-    end 
-    
 end
