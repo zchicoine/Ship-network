966fdd23b14e4d69b21ad6bf8f4145d48110e60e
pratik12 <p_bidkar@encs.concordia.ca>
2014-06-14 09:19:56 -0400

    My updated work

---
+++ ship-network/app/assets/javascripts/map_navigate.js
@@ -0,0 +1,27 @@
+//<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+$(window).load(function(){
+
+	$("#button-chevron-right").click(function(){
+	
+		 // window.map.panTo(new google.maps.LatLng(38.739391, 47.059104));
+            window.map.panBy(200,0);
+          //  control();
+            
+	});
+
+
+	$("#button-chevron-left").click(function(){
+	
+			//alert("The paragraph was clicked.");
+           window.map.panBy(-200,0);
+
+        	
+			
+	});
+
+		
+
+
+});
+
+
