d045f8c7aa4e12de56fc5f9b587a344dcd82f8d7
mehdi <elmehdi88@gmail.com>
2014-06-18 16:23:42 -0400

    db:seeds setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Destroy everything that is in the db to start from blank
+Ship.destroy_all 
+Port.destroy_all 
+
+#DateTime objects for Shipments
+require 'date'
+start_date = DateTime.new(2014,2,10)
+end_date = DateTime.new(2014,2,15)
+
+
+#Create ships by passing an array into the create method 
+montreal = Port.create!(name: 'Montreal', latitude_coordinate: 11, longitude_coordinate: 3)
+new_york = Port.create!(name: 'New York', latitude_coordinate: 2, longitude_coordinate: 6)
+vancouver = Port.create!(name: 'Vancouver', latitude_coordinate: 6, longitude_coordinate: 7)
+
+Ship.create!([{
+	name: "Ruby",
+	category: "Cargo",
+	built: "rails",
+	ports: [montreal, new_york]
+
+	},
+	{
+	name: "Clara",
+	category: "Commerce",
+	built: "metal",	
+	ports: [new_york]
+	},
+	{
+	name: "Santa",
+	category: "Cargo",
+	built: "whatever",	
+	ports: [vancouver, new_york]
+	}])
+
+
+
+p "Created #{Ship.count} ships"
