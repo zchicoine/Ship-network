057a5f80e9d3cd5ab174578f0dfdbe3e7cc00d61
malnakli <hassoun@outlook.com>
2014-06-18 20:24:51 -0400

    add some unit tests

--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -7,11 +7,11 @@ require 'rails_helper'
 
 describe Ship do 
     # before to run any test create a ship object
-    before { @port_instance = Ship.new(name: "marine prince", category: "SDBC" ,
+    before { @ship_instance = Ship.new(name: "marine prince", category: "SDBC" ,
                      built: "2012") }
     # makes @ship_instance the default subject of the test example so we don't use
     # expect(@ship_instance)
-    subject { @port_instance }
+    subject { @ship_instance }
     
     it { should respond_to(:name) }
     it { should respond_to(:category) }
@@ -19,26 +19,26 @@ describe Ship do
     it  { should be_valid }
     
     describe "when name is not present" do
-        before { @port_instance.name = " " }
+        before { @ship_instance.name = " " }
         it { should_not be_valid }
     end
     
     describe "when ship name is already stored" do
         before do
           #which creates a duplicate ship with the same attributes
-          ship_with_same_name = @port_instance
-          ship_with_same_name.name = @port_instance.name.upcase
+          ship_with_same_name = @ship_instance
+          ship_with_same_name.name = @ship_instance.name.upcase
           ship_with_same_name.save
         end
         it { should_not be_valid }
     end
 
     describe "check for invalid attributes for ship" do
-        before { 
-            @port_instance.save
+        before {
+          @ship_instance.save
         }
 
-        let(:found_ship){Ship.find_by(name: @port_instance.name)}
+        let(:found_ship){Ship.find_by(name: @ship_instance.name)}
         describe "with invalid built year" do
           let(:ship_for_invalid_built_year) { found_ship.built = 2014 }
           it { should_not eq ship_for_invalid_built_year }
@@ -52,14 +52,33 @@ describe Ship do
     describe "check for ship relationship with other tables" do
         before {
           # here should create the relationship
-          @port_instance.save
+          @ship_instance.save
         }
-        describe "check for ports relationship"do
+        describe "check for ports relationship" do
+          before {
+            @ports_array = [
+
+                  Port.create!(name: 'Ronne', latitude_coordinate: 55.08333333, longitude_coordinate: 14.68333333),
+                  Port.create!(name: 'Djibouti', latitude_coordinate: 11.6, longitude_coordinate: 43.13333333),
+                  Port.create!(name: 'Portsmouth', latitude_coordinate: 15.56666667, longitude_coordinate: -61.46666667)
+
+            ]
+
+          }
 
           describe "when ship has many ports" do
+            before {
+              @ship_instance.ports = @ports_array
+              @ship_instance.save
+            }
+            specify{ expect(@ship_instance.ports).to eq @ports_array}
 
-          end
+
+
+            end
+
         end
+
 
 
     end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,1 +1,25 @@
+=begin
+    This file is to test the shipment.rb file in app/models
+=end
+
 require 'rails_helper'
+
+
+describe Shipment do
+  # before to run any test create a ship object
+
+  three_days_from_now = Time.now + (60 * 60 * 24 * 3)
+
+  before {
+    @shipment_instance = Shipment.new(open_start_date: Time.now , open_end_date:three_days_from_now)
+  }
+
+  subject{@shipment_instance}
+
+  describe "Shipment cannot be existed by itself" do
+
+    it {should_not be_valid}
+
+   end
+
+end
