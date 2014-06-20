8c7910bcb7b8311fd66686e971866caca4c0a55c
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-06-20 15:36:53 -0400

    add attribute validation for Port and Ship

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,11 @@
 class Ship < ActiveRecord::Base
 	has_many :shipments
 	has_many :ports, :through => :shipments
+
+	validates :name,
+		presence: true
+	validates :latitude_coordinate,
+		presence: true
+	valdates :longitude_coordinate,
+		presence: true
 end
