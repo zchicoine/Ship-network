7d64eb8641e5e6b57c3af50693ec647ba75587ec
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-07-01 02:06:15 -0400

    update seed file to read port master file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,37 +2,38 @@
 Ship.destroy_all 
 Port.destroy_all 
 
-#DateTime objects for Shipments
-#require 'date'
-#start_date = DateTime.new(2014,2,10)
-#end_date = DateTime.new(2014,2,15)
+#Read .txt file of port data and add it to database
+open("db/data/port_db.txt") do |ports|  
+  ports.read.each_line do |port|  
+  	#Encode it to read foreign characters or it will not read the file.
+  	port.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '') 	
+    name, latitude, longitude = port.chomp.split(";")
+    Port.create!(name: name, latitude: latitude.to_f, longitude: longitude.to_f)  
+  end  
+end  
 
 
-#Create ships by passing an array into the create method 
-montreal = Port.create!(name: 'Montreal', latitude: 11, longitude: 3)
-new_york = Port.create!(name: 'New York', latitude: 2, longitude: 6)
-vancouver = Port.create!(name: 'Vancouver', latitude: 6, longitude: 7)
 
 Ship.create!([{
 	name: "ship 1",
 	vessel_type: "Cargo",
 	built: 15,
-	ports: [montreal, new_york]
+	#ports: [montreal, new_york]
 
 	},
 	{
 	name: "ship 2",
 	vessel_type: "Commerce",
 	built: 10,	
-	ports: [new_york]
+	#ports: [new_york]
 	},
 	{
 	name: "ship 3",
 	vessel_type: "Cargo",
 	built: 20,	
-	ports: [vancouver, new_york]
+	#ports: [vancouver, new_york]
 	}])
 
 
 
-p "Created #{Ship.count} ships"
+p "Created #{Port.count} ports"
