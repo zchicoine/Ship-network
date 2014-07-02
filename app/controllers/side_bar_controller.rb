52c770f470285c17894a0a097e9d9262ac1984cd
malnakli <hassoun@outlook.com>
2014-07-02 18:27:47 -0400

    still working

--- app/controllers/side_bar_controller.rb
+++ app/controllers/side_bar_controller.rb
@@ -1,16 +1,18 @@
 class SideBarController < ApplicationController
+    include SideBarHelper
 
     @table # to decide which table will be display
     @region_info
 
     def region
 
-        region_name = params.require(:region).permit(:name, :coordinates)
+        @region_info = params.require(:region).permit(:name, :coordinates)
+
+      # @ports_info =   ports_at_region @region_info[:name], @region_info[:coordinates]
 
 
+    #  @region_info = { name:region_info[:name]}
 
-
-      @region_info = { name:region_name[:name]}
         #redirect_to root_path
         render :partial =>  'side_bar/table_body/region'
 
--- app/helpers/side_bar_helper.rb
+++ app/helpers/side_bar_helper.rb
@@ -5,5 +5,15 @@ module SideBarHelper
         ['Capesize','Post-Panamax','Panamax','Supramax','Handymax','Handysize','Mini/MPP']
     end
 
+    # parameters :
+    # return ports name, coordinates
+    def ports_at_region region_name, region_coordinates
 
+    end
+
+
+    # return ship number by classes
+    def ships_at_region_based_on_vessel_classes region_name, region_coordinates
+
+    end
 end
