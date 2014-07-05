c24e5f4b954f89dd32fa58120361a76969bef32a
malnakli <hassoun@outlook.com>
2014-07-05 19:47:44 -0400

    finished the functionality of closing the opening part of table

--- app/assets/javascripts/side_bar.js
+++ app/assets/javascripts/side_bar.js
@@ -7,28 +7,31 @@ go_to_region = function (coordinates){
 }
 
 $(window).load(function() {
-    var visible = true;
+
+
+
     $(".triangle_image").click(function () {
+        var image = this;
+
+        if (! $(image).hasClass("this_class_only_to_change_image") ) {
+
+            $(image).attr("src", "assets/greentriangle_closed.png");
+            $(image).addClass("this_class_only_to_change_image");
+        }
+        else {
+
+            $(image).attr("src", "assets/greentriangle_down.png");
+            $(image).removeClass("this_class_only_to_change_image");
+        }
+
+        $(image).parent().parent().parent().next().children('tr').
+            closest('tr').children('td').wrapInner('<div />').
+            animate({padding: 'toggle', opacity: 'toggle'} , 150);
 
 
 
-        $('.hide_table_region').children().each(function(){
-            console.log(this);
-            $(this).animate({height: 'toggle',opacity:'toggle'}, "slow", function() {
-
-                visible = !visible;
-                if (!visible) {
-                    $(".triangle_image").attr("src", "assets/greentriangle_up.png");
-                }
-                else {
-
-                    $(".triangle_image").attr("src", "assets/greentriangle_down.png");
-
-                }
-
-
-            });
-        });
     });
+
+
 
 });
