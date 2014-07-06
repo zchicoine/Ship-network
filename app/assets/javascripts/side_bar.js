59c9170315eb796dbd021cfcb218cc631b269060
malnakli <hassoun@outlook.com>
2014-07-06 13:36:51 -0400

    closed region stats by default

--- app/assets/javascripts/side_bar.js
+++ app/assets/javascripts/side_bar.js
@@ -6,12 +6,35 @@ go_to_region = function (coordinates){
     window.map.setZoom(4);
 }
 
+closed_table_side_bar = function () {
+
+
+
+        var image = '.triangle_image';
+        console.log("works");
+        if ($(image).hasClass("closed_table")) {
+              image += ".closed_table";
+            console.log(image);
+            $(image).attr("src", "assets/greentriangle_closed.png");
+            $(image).addClass("this_class_only_to_change_image");
+            $(image).parent().parent().parent().next().children('tr').
+                closest('tr').children('td').wrapInner('<div />').
+                animate({padding: 'toggle', opacity: 'toggle'}, 1);
+        }
+
+
+
+
+
+}
+
+
 
 
 
     $(document).on('click','.triangle_image',function () {
         var image = this;
-        console.log("works");
+
         if (! $(image).hasClass("this_class_only_to_change_image") ) {
 
             $(image).attr("src", "assets/greentriangle_closed.png");
