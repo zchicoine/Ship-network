efc73bb42c0d6bf3b418efcaaf0e7142663bb08f
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-07-16 15:50:08 -0400

    add validations for Ship and Port

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,4 +1,11 @@
 class Port < ActiveRecord::Base
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  #validates_numericality_of :latitude
+  validates_inclusion_of :latitude, in:-90..90
+  #validates_numericality_of :longitude
+  validates_inclusion_of :longitude, in:-180..180
+
 	has_many :shipments
 	has_many :ships, :through => :shipments, :dependent => :destroy
 end
