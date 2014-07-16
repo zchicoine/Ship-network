ef64373392815caedb717c83d0b106b19ee13920
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-07-16 18:37:58 -0400

    add enum for vessel_class

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -5,6 +5,7 @@ class Ship < ActiveRecord::Base
   validates_numericality_of :deadweight, :greater_than => 0
 
   enum vessel_type: [:notype, :sdbc, :ohbs , :mpp, :tween, :roro]
+  enum vessel_class: [:No_Type, :MiniBulker, :Handysize, :Handymax , :Supramax, :Panamax, :PostPanamax, :Capesize]
 
 	has_many :shipments
 	has_many :ports, :through => :shipments, :dependent => :destroy
