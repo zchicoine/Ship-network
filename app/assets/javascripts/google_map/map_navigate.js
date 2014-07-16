da09a9891c0223175e4433dfc67896669e87907d
pratik12 <p_bidkar@encs.concordia.ca>
2014-07-16 14:59:05 -0400

    implemented adding control to right navigate to scroll through regions at the points mentioned by zack

--- app/assets/javascripts/google_map/map_navigate.js
+++ app/assets/javascripts/google_map/map_navigate.js
@@ -1,4 +1,17 @@
 //<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+var region_layer_array = [
+				new google.maps.LatLng(52.370216,4.895168),
+//new orleans
+new google.maps.LatLng(29.951066,-90.071532),
+//Recalada
+new google.maps.LatLng(-34.786878,-55.883175),
+//lagos
+new google.maps.LatLng(6.524379,3.379206),
+//muskat
+new google.maps.LatLng(23.610000,58.540000)
+//
+];
+var i=0;
 $(window).load(function(){
 
 	$("#outer-map").click(function(e){
@@ -7,9 +20,21 @@ $(window).load(function(){
 	});
 
 	$("#right-img-responsive").click(function(){	
-		 // window.map.panTo(new google.maps.LatLng(38.739391, 47.059104));
-            window.map.panBy(150,0);
-          //  control();
+		if(this.id=='right-img-responsive' && i!=(location_array.length-1))
+			{
+
+			window.map.setCenter(region_layer_array[i]);
+			window.map.setZoom(3);
+			i++;
+
+		}
+		else if(i==(location_array.length-1)){
+			window.map.setCenter(region_layer_array[(region_layer_array.length-1)]);
+			window.map.setZoom(3);
+			i=0;
+			
+		}
+		
 	});
 
 
