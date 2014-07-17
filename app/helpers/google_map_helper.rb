58c1cf593825504340217b3b6fd84e857f94667f
malnakli <hassoun@outlook.com>
2014-07-17 15:02:29 -0400

    refactoring and optimizing the side table calling backend

--- app/helpers/google_map_helper.rb
+++ app/helpers/google_map_helper.rb
@@ -29,18 +29,18 @@ module GoogleMapHelper
     def get_total_number_of_ship
         Ship.all.size
     end
-    def get_number_of_ships_per_class category_name
-      @ships_per_class = Ship.select(:vessel_class).where(vessel_class: category_name).size
-    end
+    # def get_number_of_ships_per_class category_name
+    #   @ships_per_class = Ship.select(:vessel_class).where(vessel_class: category_name).size
+    # end
 
-    def get_number_of_ships_per_region region_name
-      @ships_per_region = Port.includes(:ships).where(region: region_name)
-      @ship_count = 0
-      @ships_per_region.each do |port|
-        @ship_count += port.ships.size
-      end
-      @ship_count
-    end
+    # def get_number_of_ships_per_region region_name
+    #   @ships_per_region = Port.includes(:ships).where(region: region_name)
+    #   @ship_count = 0
+    #   @ships_per_region.each do |port|
+    #     @ship_count += port.ships.size
+    #   end
+    #   @ship_count
+    # end
 
     def get_number_of_ships_per_region_per_class region_name , category_name
         @ships_per_region = Port.includes(:ships).where(region: region_name)
