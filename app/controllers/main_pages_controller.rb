d7c3d2e6a95a940e2d40e9d15d84a8fd712bcd65
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-07-26 18:53:56 -0400

    add function for ports names and coordinates

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -3,15 +3,19 @@
 =end
 
 class MainPagesController < ApplicationController
-  
-    def index
 
+
+    def index
+       @ship_sum =  ShipBLL.new
+       @ship_sum = @ship_sum.get_total_number_of_ship
     end
     def loginpage
         respond_to do |format|
             format.html {render :partial =>  'main_pages/login'}
             format.js {render 'main_pages/js/login'}
         end
-
     end
+
+
+
 end
