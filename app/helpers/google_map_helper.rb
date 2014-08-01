7100e935ea5c6721fdc40927c2a096e6c095cdfd
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-07-31 23:17:43 -0400

    add helper method and modify ship_details view

--- app/helpers/google_map_helper.rb
+++ app/helpers/google_map_helper.rb
@@ -17,10 +17,33 @@ module GoogleMapHelper
 
     end
 
-    def get_ship_details ship_name = "null"
+    def get_ship_details_first_table ship_name = "null"
         @ship_id = Ship.find_by_name(ship_name).id
-        @return_ship_details = ShipDetail.find_by(ship_id: @ship_id)
+        @return_ship_details = ShipDetail.select(:draft, :built, :tons_per_centimeter, :flag,
+                                                 :classification_society, :length_over_all, :beam, :holds).find_by(ship_id: @ship_id)
     end
+
+    def get_ship_details_second_table ship_name = "null"
+      @ship_id_two = Ship.find_by_name(ship_name).id
+      @return_ship_details_two = ShipDetail.select(:hatches, :gross_registered_tonnage, :net_registered_tonnage, :total_cubic_meters_GR,
+                         :total_cubic_meters_BL, :total_cubic_feet_GR, :total_cubic_feet_BL, :intermediate_fuel_oil_180?, :intermediate_fuel_oil_380?).find_by(ship_id: @ship_id)
+    end
+
+    # To make just one query to the database
+
+    #def get_ship_details_first_table ship_name = "null"
+     # @ship_id = Ship.find_by_name(ship_name).id
+      #@all_ship_details = ShipDetail.find_by(ship_id: @ship_id)
+      #@return_ship_details_table_one = @all_ship_details.pluck(:draft, :built, :tons_per_centimeter, :flag,
+       #                                        :classification_society, :length_over_all, :beam, :holds)
+
+      #@return_ship_details_table_two = @all_ship_details.select(:hatches, :gross_registered_tonnage, :net_registered_tonnage, :total_cubic_meters_GR,
+                                                 #:total_cubic_meters_BL, :total_cubic_feet_GR, :total_cubic_feet_BL, :intermediate_fuel_oil_180?, :intermediate_fuel_oil_380?)
+
+      #@table_details_array = [@return_ship_details_table_one, @return_ship_details_table_two]
+   # end
+
+
 
 end
 
