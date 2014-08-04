ece109fdac5009a7d1289d17e3476daa528af6e7
malnakli <hassoun@outlook.com>
2014-08-04 12:01:29 -0400

    fix migrate to works with Heroku database

--- db/migrate/20140716221431_change_vessel_class_format_in_ship.rb
+++ db/migrate/20140716221431_change_vessel_class_format_in_ship.rb
@@ -1,5 +1,5 @@
 class ChangeVesselClassFormatInShip < ActiveRecord::Migration
   def change
-    change_column :ships, :vessel_class, :integer, default: 0
+    change_column :ships, :vessel_class, :integer, default: 0 #'integer USING CAST(vessel_type AS integer)'
   end
 end
--- db/migrate/20140719203128_add_deadweight_capacity_to_ships.rb
+++ db/migrate/20140719203128_add_deadweight_capacity_to_ships.rb
@@ -1,5 +1,5 @@
 class AddDeadweightCapacityToShips < ActiveRecord::Migration
   def change
-    add_column :ships, :deadweight_cargo_capacity, :integer
+    add_column :ships, :deadweight_cargo_capacity, :integer #'integer USING CAST(vessel_type AS integer)'
   end
 end
