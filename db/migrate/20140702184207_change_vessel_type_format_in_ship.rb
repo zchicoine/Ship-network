5555892d467976afd3abd451b3fcf6e29b65e3c2
malnakli <hassoun@outlook.com>
2014-08-04 19:55:13 -0400

    still fix migrate to works with Heroku database 3

--- db/migrate/20140702184207_change_vessel_type_format_in_ship.rb
+++
@@ -1,5 +0,0 @@
-class ChangeVesselTypeFormatInShip < ActiveRecord::Migration
-    def change
-        change_column :ships, :vessel_type,'integer USING CAST(vessel_type AS integer)', default: 0
-    end
-end
--- db/migrate/20140716221431_change_vessel_class_format_in_ship.rb
+++
@@ -1,5 +0,0 @@
-class ChangeVesselClassFormatInShip < ActiveRecord::Migration
-  def change
-    change_column :ships, :vessel_class, 'integer USING CAST(vessel_class AS integer)', default: 0
-  end
-end
