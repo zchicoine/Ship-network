508f2c635111783dc257dd3be640f5da4665be53
malnakli <hassoun@outlook.com>
2014-08-04 20:42:40 -0400

    still fix migrate to works with Heroku database 4

--- db/migrate/20140730192323_remove_fields_from_ships.rb
+++
@@ -1,8 +0,0 @@
-class RemoveFieldsFromShips < ActiveRecord::Migration
-  def change
-    remove_column :ships, :built
-    remove_column :ships, :draft
-    remove_column :ships, :beam
-    remove_column :ships, :loa
-  end
-end
--- db/migrate/20140804235326_change_vessel_to_integer.rb
+++
@@ -1,7 +0,0 @@
-class ChangeVesselToInteger < ActiveRecord::Migration
-  def change
-      change_column :ships, :vessel_type,'integer USING CAST(vessel_type AS integer)', default: 0
-      change_column :ships, :vessel_category, 'integer USING CAST(vessel_category AS integer)', default: 0
-
-  end
-end
