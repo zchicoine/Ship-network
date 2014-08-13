032fc3195ea94efa70ef9b73cbbafbf889145f48
malnakli <hassoun@outlook.com>
2014-08-13 12:53:06 -0400

    add error message when username or password is incorrect

--- app/controllers/brokers/sessions_controller.rb
+++ app/controllers/brokers/sessions_controller.rb
@@ -7,22 +7,29 @@ class Brokers::SessionsController < Devise::SessionsController
         clean_up_passwords(resource)
         respond_with(resource, serialize_options(resource)) do |format|
             format.html {render :partial =>  'main_pages/login'}
-            format.js {render 'main_pages/js/login' }
+            format.js {render 'main_pages/js/login' , :locals => { error: nil}}
         end
     end
 
     # POST /resource/sign_in
     def create
 
-            self.resource = warden.authenticate!(auth_options)
-            set_flash_message(:notice, :signed_in) if is_flashing_format?
-            sign_in(resource_name, resource)
-            yield resource if block_given?
-           # respond_with resource, location: after_sign_in_path_for(resource)
-            respond_with(resource) do |format|
-                format.html {'/'}
-                format.js {render 'main_pages/js/sign_in'}
-            end
+       broker =  catch :warden do
+               self.resource = warden.authenticate!(auth_options)
+        end
+           if broker.is_a? Broker
+               set_flash_message(:notice, :signed_in) if is_flashing_format?
+               sign_in(resource_name, resource)
+               yield resource if block_given?
+               respond_with(resource) do |format|
+                   format.html {root_path}
+                   format.js {render 'main_pages/js/sign_in'}
+               end
+           else
+               render 'main_pages/js/login', :locals => { error: "invaild username or password" }
+           end
+
+
 
     end
 end
