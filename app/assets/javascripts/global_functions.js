85f6f517b7ad9eeaea7a830358c2d8e7c9dd90be
malnakli <hassoun@outlook.com>
2014-08-14 14:48:15 -0400

    still working on popover not finished yet

--- app/assets/javascripts/global_functions.js
+++ app/assets/javascripts/global_functions.js
@@ -33,14 +33,47 @@ update_ship_view = function(ship_name){
 }
 
 
-load_popover_with_id = function(load, content_id){
-    id = "#" + content_id;
-    var inText = $(id).html();
-    $(load).popover( {
-        // trigger: 'focus',
-        html: true,
-        content: inText
-    });
-
-}
+//load_popover_with_id = function(load, content_id, placement){
+//    id = "#" + content_id;
+//    var inText = $(id).html();
+//    console.log("load_popover");
+//    $(load).popover( {
+//        trigger: 'click',
+//        html: true,
+//        placement: placement,
+//        content: inText
+//    });
+//    $('.broker_info').on('show.bs.popover', function () {
+//        // console.log("popover_focus_for_tr_element click" + i++);
+//        popover_show = true;
+//    });
+//    $('.broker_info').on('hidden.bs.popover', function () {
+//        // console.log("popover_focus_for_tr_element click" + i++);
+//        popover_show = false;
+//    });
+//}
+//
+//    var popover_show = false;
+//    $(document).on('blur', ".popover_blur_for_tr_element" , function () {
+//
+//           i = 0;
+//        console.log(" click - " + popover_show);
+//        if(popover_show ){
+//            $("*").popover('hide');
+//        }else{
+//
+//        }
+//
+//
+//
+//
+//
+//    });
+////
+//    $(document).on('focus',".popover_focus_for_tr_element", function () {
+//
+//        console.log("popover_focus_for_tr_element blur");
+//
+//        // $('*').popover('hide');
+//    });
 
