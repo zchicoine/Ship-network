a837aa80ddff432c07be5e0174140b965d5309be
malnakli <hassoun@outlook.com>
2014-08-16 15:06:54 -0400

    fix the popover focus and click in order to make both work together

--- app/assets/javascripts/global_functions.js
+++ app/assets/javascripts/global_functions.js
@@ -33,47 +33,111 @@ update_ship_view = function(ship_name){
 }
 
 
-//load_popover_with_id = function(load, content_id, placement){
-//    id = "#" + content_id;
-//    var inText = $(id).html();
-//    console.log("load_popover");
-//    $(load).popover( {
-//        trigger: 'click',
-//        html: true,
-//        placement: placement,
-//        content: inText
-//    });
-//    $('.broker_info').on('show.bs.popover', function () {
-//        // console.log("popover_focus_for_tr_element click" + i++);
+
+
+
+
+
+// fix the focus and click of bootstrap by the following code
+
+var popover_show = false;
+var popover_called_once = undefined; // to initialize the popover only once
+
+
+load_popover_with_id = function(element,popover_load, content_id, placement) {
+
+    if (!  element.isSameNode(popover_called_once) ) {
+
+        popover_called_once = element;
+
+        id = "#" + content_id;
+
+        var content = $(id).html();
+
+        console.log("load_popover");
+        $(popover_load).popover({
+            trigger: 'manual',
+            html: true,
+            placement: placement,
+            content: content
+        });
+
+        // on blur
+            $(element).blur(function () {
+            console.log("hidden popover blur" );
+
+            if( popover_show){
+                $(popover_load).popover('hide');
+                $(element).trigger('click');
+            }
+
+        });
+        // on click
+        $(element).clickToggle(function () {
+
+            console.log("show popover " + "click 1");
+            if(! popover_show){
+                $(popover_load).popover('show');
+
+            }
+
+        },function (be) {
+            console.log("hidden popover " + "click 2");
+            if(popover_show){
+                $(popover_load).popover('hide');
+            }
+
+        } );
+
+    }
+    $(popover_load).on('show.bs.popover', function () {
+
+        popover_show = true;
+    });
+    $(popover_load).on('hide.bs.popover', function () {
+
+        popover_show = false;
+    });
+
+}
+
+
+
+
+
+//    $(element).on('shown.bs.popover', function () {
+//
+//
 //        popover_show = true;
 //    });
-//    $('.broker_info').on('hidden.bs.popover', function () {
-//        // console.log("popover_focus_for_tr_element click" + i++);
+//    $(element).on('hidden.bs.popover', function () {
+//         console.log("hidden popover");
 //        popover_show = false;
 //    });
-//}
+
+//    $(element).click( function () {
 //
-//    var popover_show = false;
-//    $(document).on('blur', ".popover_blur_for_tr_element" , function () {
-//
-//           i = 0;
-//        console.log(" click - " + popover_show);
+//        console.log(element.className +   " click - " + popover_show);
 //        if(popover_show ){
-//            $("*").popover('hide');
+//            $(popover_load).popover('hide');
 //        }else{
 //
 //        }
 //
-//
-//
-//
-//
 //    });
-////
+
+
+
+
+//
 //    $(document).on('focus',".popover_focus_for_tr_element", function () {
 //
 //        console.log("popover_focus_for_tr_element blur");
 //
 //        // $('*').popover('hide');
 //    });
+//
+
+
+
 
