795f753fcc30a287e3cdacfd266b21655b593544
malnakli <hassoun@outlook.com>
2014-08-16 23:53:32 -0400

    add broker company instead of the name

--- app/models/BLL/shipment_bll.rb
+++ app/models/BLL/shipment_bll.rb
@@ -11,7 +11,7 @@ class ShipmentBLL < Shipment
                                                     "ports.name" => port_name,name:ship_name).execute_query 1
 
         unless result.blank?
-                coverted_to_ruby_hash = {broker_name: result[:username], broker_email: result[:email],deadweight: result[:deadweight],open_start_date: result[:open_start_date],
+                coverted_to_ruby_hash = {broker_name: result[:username], broker_email: result[:email],broker_company:result[:company],deadweight: result[:deadweight],open_start_date: result[:open_start_date],
                                          open_end_date:  result[:open_end_date], vessel_category: result[:vessel_category],
                                          ship_name:  ship_name ,  port_name:  port_name
                                         }
