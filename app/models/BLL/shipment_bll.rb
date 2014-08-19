ebcf6177028b8382794d844e1f33c87279fb349f
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-08-19 19:28:56 -0400

    add deadweight cargo

--- app/models/BLL/shipment_bll.rb
+++ app/models/BLL/shipment_bll.rb
@@ -7,11 +7,11 @@ class ShipmentBLL < Shipment
         #  return {value: result/0, error: nil/message}
     def get_shipCategory_deadweight_brokerName_openStartDate_and_endDate ship_name = "", port_name = ""
         result = ShipBLL.joins(:ports, :shipments => [:brokers]).select("brokers.*",
-                                "shipments.open_start_date","shipments.open_end_date",:deadweight,:vessel_category).where(
+                                "shipments.open_start_date","shipments.open_end_date",:deadweight_cargo_capacity, :deadweight,:vessel_category).where(
                                                     "ports.name" => port_name,name:ship_name).execute_query 1
 
         unless result.blank?
-                coverted_to_ruby_hash = {broker_name: result[:username], broker_email: result[:email],deadweight: result[:deadweight],open_start_date: result[:open_start_date],
+                coverted_to_ruby_hash = {broker_name: result[:username], broker_email: result[:email], deadweight_cargo_capacity: result[:deadweight_cargo_capacity], deadweight: result[:deadweight],open_start_date: result[:open_start_date],
                                          open_end_date:  result[:open_end_date], vessel_category: result[:vessel_category],
                                          ship_name:  ship_name ,  port_name:  port_name
                                         }
