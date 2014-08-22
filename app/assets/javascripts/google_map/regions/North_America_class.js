b83c7289ff1633078b7736e7078817b799b6afa8
malnakli <hassoun@outlook.com>
2014-08-21 23:17:41 -0400

    remove console.log

--- app/assets/javascripts/google_map/regions/North_America_class.js
+++ app/assets/javascripts/google_map/regions/North_America_class.js
@@ -16,6 +16,5 @@ North_America_class.prototype.change_region_view = function() {
     window.map.setCenter(new google.maps.LatLng(48.2893, -99.3594));
     window.map.setZoom(3);
     update_region_view(this.name);
-    console.log("mo");
 };
 
