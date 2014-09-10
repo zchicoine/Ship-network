b54818004f7e4ac9eec44da9805e8ecfd027e989
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2014-09-09 20:30:55 -0400

    add open_port and open_date to action

--- app/controllers/ship_details_controller.rb
+++ app/controllers/ship_details_controller.rb
@@ -3,7 +3,7 @@ class ShipDetailsController < ApplicationController
 
     def show
         @name_of_ship,@name_of_port,@name_of_region = params[:ship_name],params[:port_name],params[:region_name]
-
+        @get_open_port_and_date = UnitOfWork.instance.shipment.get_open_port_and_date @name_of_ship, @name_of_port
         respond_to do |format|
             format.html {render :partial =>  'ship_details/index'}
             format.js { render 'ship_details/js/index'}
