51ebbbd076c18496e3a46c957b4109326cb2009e
malnakli <hassoun@outlook.com>
2014-11-14 10:10:47 -0500

    fix the reload map when motor vessel window close by clicking close button

--- app/assets/javascripts/map_subsystem/google_map/google_map.js
+++ app/assets/javascripts/map_subsystem/google_map/google_map.js
@@ -34,36 +34,3 @@ function initialize_the_map() {
 
 // create an instance of google map app
 var GoogleMapAppInstance = new GoogleMapApp();
-
-
-
-/*
-
-specification format = {new_map: false/true
-                        layer: GLOBAL_LEVEL/REGION_LEVEL/PORT_LEVEL,
-                        region_name: global/region_name,
-                        port_name:
-
-                          }
-*/
-function reload_the_map_with_specification(specification){
-
-
-    if(specification != undefined){
-
-        MAP.Controller.current_zoom_layer.value = specification['layer'];
-
-        if(specification['new_map'] == true){
-
-         var  map = MAP.google_map(specification['new_map']);
-
-        }
-         if(specification['layer'] == PORT_LEVEL  && specification['region_name'] != undefined){
-             region_objects_variable.each_object().set_map_label(map);
-             MAP.google_controller_methods.display_ports(specification['region_name']);
-             default_map_navigate(specification['region_name']);
-
-        }
-    }
-}
-
