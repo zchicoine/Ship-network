6038bd3f1adebb1589c56bd5ddbd3ed3a7e31a73
malnakli <hassoun@outlook.com>
2014-12-21 20:32:57 -0500

    add pluralize_word function in helper_function.js

--- app/assets/javascripts/helper/helper_function.js
+++ app/assets/javascripts/helper/helper_function.js
@@ -15,3 +15,17 @@ var remove_white_space = function(name){
 
 };
 
+/**
+ * number: an integer
+ * word: the word that need to be pluralized
+ * pword: the pluralize form of the word
+ * return the word either pluralize or not based on the value of the number
+ */
+var pluralize_word = function(number,word,pword)
+{
+    if(string_match(word) && string_match(pword))
+    if(number > 1 && ! isNaN(number))
+        return pword;
+
+    return word;
+}
