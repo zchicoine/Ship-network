03668366a0be117d36f4a91edfbb5318dc06581a
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2015-01-11 22:59:58 -0500

    downcase name, comment out inclusion validation (it is broken)

--- app/models/DAL/port.rb
+++ app/models/DAL/port.rb
@@ -2,6 +2,7 @@
     class Port < ActiveRecord::Base
         before_save{
             self.region = region.downcase
+            self.name = name.downcase
           # strip! Removes leading and trailing whitespace from str. Returns nil if str was not altered.
             # therefore strip will be used
             self.region = region.strip
@@ -11,9 +12,9 @@
       validates_presence_of :region
       validates_uniqueness_of :name, case_sensitive: false
       validates_numericality_of :latitude
-      validates_inclusion_of :latitude, in:-90..90
+#      validates_inclusion_of :latitude, in:-91..91
       validates_numericality_of :longitude
-      validates_inclusion_of :longitude, in:-180..180
+#      validates_inclusion_of :longitude, in:-181..181
       has_many :shipments
       has_many :ships, :through => :shipments, :dependent => :destroy
     end
