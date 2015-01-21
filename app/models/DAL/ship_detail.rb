1d1e752834bd894bfc6f585c3ac683cc7d36225b
El-Mehdi Beghdadi <elmehdi88@gmail.com>
2015-01-21 02:55:45 -0500

    add option (in ship model) for ship_details to be updated at the same time as ship object

--- app/models/DAL/ship_detail.rb
+++ app/models/DAL/ship_detail.rb
@@ -1,4 +1,5 @@
 class ShipDetail < ActiveRecord::Base
   belongs_to :ship
-    validates :built, numericality: {less_than_or_equal_to: Time.now.year}
+
+  validates :built, numericality: {less_than_or_equal_to: Time.now.year}
 end
