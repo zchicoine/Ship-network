6e5aa74bf363e16d900ac3d3141afed1a9e704b1
Mohammed alnakli <hassoun@outlook.com>
2015-05-13 12:46:50 -0400

    add a comment to main.js file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,10 @@
+/**
+ Define one global var.
+ the following is taken from "Maintainable JavaScript by Nicholas Zakas(O’Reilly). Copyright 2012 Nicholas Zakas, 978-1-449-32768-2"
+*/
 
-
-
-$(document).ready(function(){
+$(document).ready(function ()
+{
 
 
     MainViewGeneratorInstance.globalView();
