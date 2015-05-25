0f98dd160adaec48630b5f3eccc4e150cb50c62f
malnakli <hassoun@outlook.com>
2015-05-25 07:11:41 -0400

    update application and Gemfile.lock

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ShipNetwork
   class Application < Rails::Application
-    include Kee
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
