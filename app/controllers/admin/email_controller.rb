54fa829478c07fb657ec1217cebdd0ed5d57c577
Mohammed alnakli <hassoun@outlook.com>
2015-06-04 15:42:53 -0400

    add pagination  to email view

--- app/controllers/admin/email_controller.rb
+++ app/controllers/admin/email_controller.rb
@@ -3,12 +3,11 @@
 ###
 class Admin::EmailController < ApplicationController
     include  AdminHelperC
-
     layout 'admin'
     before_action :require_admin_authentication #the admin_helpers controller has to be protected to only let admins in.
 
     def index
-        @emails = ShipEmailBLL.all
+        @emails = ShipEmailBLL.paginate(page: params[:page])
         render ('index')
     end
 
